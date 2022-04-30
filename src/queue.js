56eb59aabb71ab9a2ce72c736bb4f8bea664710b
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-30 16:50:24 +0300

    feat:  src/queue.js

--- src/queue.js
+++ src/queue.js
@@ -3,6 +3,16 @@ const { NotImplementedError } = require('../extensions/index.js');
 // const { ListNode } = require('../extensions/list-node.js');
 
 /**
+ * 
+ * =================================================================
+ * 
+ * 
+ * https://www.youtube.com/watch?v=NpcHTBOAId0
+ * 
+ * 
+ * 
+ * 
+ * =================================================================
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
  * @example
@@ -13,23 +23,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
 class Queue {
+  constructor() {
+    this.element = null
+    this.elLength = 0
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    return this.element
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    if (this.element === null) {
+      this.element = new ListNode(value)
+    } else {
+      let currentEl = this.element
+
+      while (currentEl.next) {
+        currentEl = currentEl.next
+      }
+
+      currentEl.next = new ListNode(value)
+    }
+    this.elLength++;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    let currentEl = this.element
+    this.element = currentEl.next
+    this.elLength--
+    return currentEl.value
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
+
 
 module.exports = {
   Queue
