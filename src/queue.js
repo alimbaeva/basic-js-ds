64bbe510b9d0064f02506b6a86acdfcac7f587fa
Asel <asel.alimbaeva.kk@gmail.com>
2023-12-19 15:26:17 +0300

    refactore: Queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * 
@@ -23,52 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class ListNode {
-  constructor(x) {
-    this.value = x;
-    this.next = null;
-  }
-}
+
 class Queue {
   constructor() {
-    this.element = null
-    this.elLength = 0
-  }
-
-  getUnderlyingList() {
-    return this.element
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.head = null;
+    this.tail = null;
   }
 
   enqueue(value) {
-    if (this.element === null) {
-      this.element = new ListNode(value)
+    const newNode = new ListNode(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      let currentEl = this.element
-
-      while (currentEl.next) {
-        currentEl = currentEl.next
-      }
-
-      currentEl.next = new ListNode(value)
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    this.elLength++;
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   dequeue() {
-    let currentEl = this.element
-    this.element = currentEl.next
-    this.elLength--
-    return currentEl.value
-    // throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const removedValue = this.head.value;
+    this.head = this.head.next;
+
+    if (this.head === null) {
+      this.tail = null; // Если элемент был последним в очереди
+    }
+
+    return removedValue;
+  }
+
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  isEmpty() {
+    return this.head === null;
   }
 }
-
 
 module.exports = {
   Queue
 };
+ 
